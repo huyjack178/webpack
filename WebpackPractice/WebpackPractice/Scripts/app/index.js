df4d8e75699754daecff352b5d5c3ced1edc1f52
Huy Anh Nguyen <huy.anh.nguyen178[gmail.com>
2016-08-03 06:53:10 +0700

    update

--- WebpackPractice/WebpackPractice/Scripts/app/index.js
+++ WebpackPractice/WebpackPractice/Scripts/app/index.js
@@ -1,5 +1,6 @@
 ﻿import jQuery from '../jquery-1.10.2.js';
 import '../../Content/Index.css';
+import {message} from './message.js';
 
 (function ($) {
     $("p").text(message.GetMessageIndex());
--- WebpackPractice/WebpackPractice/Scripts/app/message.js
+++ WebpackPractice/WebpackPractice/Scripts/app/message.js
@@ -12,3 +12,21 @@
         }
     }
 }();
+export {message as message};
+
+var messageExtension = function () {
+    var messageIndex = "extension - this is index page";
+    var messageAbout = "extension - this is about page";
+
+    return {
+        GetMessageIndex: function () {
+            return messageIndex;
+        },
+
+        GetMessageAbout: function () {
+            return messageAbout;
+        }
+    }
+}();
+
+export {messageExtension as messageExt}
